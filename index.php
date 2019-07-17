1784d7afec6cb9d8038d0f5a200714057e857df6
encrypter-manyange <44680527+encrypter-manyange@users.noreply.github.com>
2019-07-17 18:16:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    We will resume services from tommorow we work 0800hrs to 1702hrs. To handle outgoing request. 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-1639187849843346",
+    enable_page_level_ads: true
+  });
+</script>
+  </body>
+</html>
